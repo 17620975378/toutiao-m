7ff02e575529abce9a799cae0c4292e7299c12b7
aaa <limin@aaaaadeMacBook-Pro.local>
2021-11-17 17:34:25 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,19 @@
     <!-- 路由出口 -->
     <router-view/>
     <h1>黑马头条</h1>
-  </div>
+    <div>
+      <i class="toutiao toutiao-shouye"></i>
+    </div>
+
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="info">信息按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+
+  <van-button plain type="primary">朴素按钮</van-button>
+  <van-button plain type="info">朴素按钮</van-button>
+</div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-//加载全局样式
-import './style/index.less'
+import Vant from 'vant' // 加载Vant核心组件库
+import 'vant/lib/index.css' // 加载Vant全局样式
+
+
+import './style/index.less' // 加载全局样式
+
+Vue.use(Vant) // 注册使用Vant组件库
 
 Vue.config.productionTip = false
 
